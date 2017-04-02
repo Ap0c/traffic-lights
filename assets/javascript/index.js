d9e6736fffdec2c808dafc8520d39396bcbec104
Ap0c <Ap0c@users.noreply.github.com>
2017-04-02 14:45:36 +0100

    Added buttons to set the traffic light state.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -7,6 +7,12 @@ import { render } from 'react-dom';
 // ----- Setup ----- //
 
 const SELECTED_LIGHT = 'traffic-light__light--selected';
+const BUTTONS_STATES = {
+	go: 'GO',
+	stop: 'STOP',
+	going: 'GOING',
+	stopping: 'STOPPING'
+};
 
 
 // ----- Functions ----- //
@@ -23,6 +29,8 @@ function lightClasses (lightState) {
 			return { red: SELECTED_LIGHT, amber: SELECTED_LIGHT, green: '' };
 		case ('STOPPING'):
 			return { red: '', amber: SELECTED_LIGHT, green: '' };
+		default:
+			return {};
 
 	}
 
@@ -33,19 +41,67 @@ function lightClasses (lightState) {
 
 function TrafficLight (props) {
 
-	let classes = lightClasses(props.state);
+	let classes = lightClasses(props.stage);
 
 	return (
 		<figure className="traffic-light">
 			<div className={`traffic-light__light ${classes.red}`}></div>
-			<div className={`traffic-light__light ${classes.red}`}></div>
+			<div className={`traffic-light__light ${classes.amber}`}></div>
 			<div className={`traffic-light__light ${classes.green}`}></div>
 		</figure>
 	);
 
 }
 
+function Buttons (props) {
+
+	const buttonData = [
+		{ text: 'Go', value: 'go' },
+		{ text: 'Stopping', value: 'stopping' },
+		{ text: 'Stop', value: 'stop' },
+		{ text: 'Going', value: 'going' }
+	];
+
+	const buttons = buttonData.map(data => {
+
+		const attrs = {
+			className: 'buttons__button',
+			key: data.value,
+			onClick: () => props.onClick(data.value)
+		};
+
+		return <button {...attrs}>{data.text}</button>;
+
+	});
+
+	return <div className="buttons">{buttons}</div>;
+
+}
+
+class App extends React.Component {
+
+	constructor (props) {
+		super(props);
+		this.state = { stage: props.stage };
+		this.handleButtonClick = this.handleButtonClick.bind(this);
+	}
+
+	handleButtonClick (button) {
+		this.setState({ stage: BUTTONS_STATES[button] });
+	}
+
+	render () {
+		return (
+			<div className="main-wrapper">
+				<TrafficLight stage={this.state.stage} />
+				<Buttons onClick={this.handleButtonClick} />
+			</div>
+		);
+	}
+
+}
+
 
 // ----- Render ----- //
 
-render(<TrafficLight state="GO" />, document.getElementById('app'));
+render(<App stage="GO" />, document.getElementById('app'));
