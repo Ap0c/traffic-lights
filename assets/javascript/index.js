c5a1236dfbfe29a43ffc1fe4c03b8e02d9c13755
Ap0c <Ap0c@users.noreply.github.com>
2017-04-02 12:44:21 +0100

    Made lights set colour based on state.

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -4,15 +4,42 @@ import React from 'react';
 import { render } from 'react-dom';
 
 
+// ----- Setup ----- //
+
+const SELECTED_LIGHT = 'traffic-light__light--selected';
+
+
+// ----- Functions ----- //
+
+function lightClasses (lightState) {
+
+	switch (lightState) {
+
+		case ('GO'):
+			return { red: '', amber: '', green: SELECTED_LIGHT };
+		case ('STOP'):
+			return { red: SELECTED_LIGHT, amber: '', green: '' };
+		case ('GOING'):
+			return { red: SELECTED_LIGHT, amber: SELECTED_LIGHT, green: '' };
+		case ('STOPPING'):
+			return { red: '', amber: SELECTED_LIGHT, green: '' };
+
+	}
+
+}
+
+
 // ----- Components ----- //
 
 function TrafficLight (props) {
 
+	let classes = lightClasses(props.state);
+
 	return (
 		<figure className="traffic-light">
-			<div className="traffic-light__light">Red</div>
-			<div className="traffic-light__light">Orange</div>
-			<div className="traffic-light__light">Green</div>
+			<div className={`traffic-light__light ${classes.red}`}></div>
+			<div className={`traffic-light__light ${classes.red}`}></div>
+			<div className={`traffic-light__light ${classes.green}`}></div>
 		</figure>
 	);
 
@@ -21,4 +48,4 @@ function TrafficLight (props) {
 
 // ----- Render ----- //
 
-render(<TrafficLight />, document.getElementById('app'));
+render(<TrafficLight state="GO" />, document.getElementById('app'));
