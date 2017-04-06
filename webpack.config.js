1b506f0fc483cb4242db931f04dc20976a3dad2d
Jamie Byers <james.byers@guardian.co.uk>
2017-04-06 11:32:14 +0100

    Commented the webpack config to (hopefully) make it less confusing.

--- webpack.config.js
+++ webpack.config.js
@@ -7,18 +7,23 @@ const ExtractTextPlugin = require('extract-text-webpack-plugin');
 // ----- Config ----- //
 
 module.exports = {
+    // Individual input files for each page.
     entry: {
     	css: './assets/stylesheets/main.scss',
     	react: './assets/javascript/react/react.js',
     	react_redux: './assets/javascript/react_redux/react_redux.js',
     	react_redux_flow: './assets/javascript/react_redux_flow/react_redux_flow.js'
     },
+    // Outputs each input file to the build dir.
     output: {
 		filename: '[name].js',
 		path: path.join(__dirname, 'build')
     },
     module: {
 		rules: [
+            // Applies babel with react and flow to all js files.
+            // Babel is needed to handle JSX.
+            // Flow preset strips out type information for the browser-side js.
 		    {
 				test: /\.js$/,
 				loader: 'babel-loader',
@@ -27,18 +32,22 @@ module.exports = {
 				    presets: ['react', 'flow']
 				}
 		    },
+            // Compiles SASS, and strips the CSS out into a separate file.
 		    {
 		    	test: /\.scss$/,
 		    	loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
 		    }
 		]
     },
+    // Tells webpack to use preact in place of react.
     resolve: {
     	alias: {
     		'react': 'preact-compat',
     		'react-dom': 'preact-compat'
     	}
     },
+    // This is what pulls the CSS out into its own file.
+    // Will generate a random, empty css.js file.
     plugins: [
     	new ExtractTextPlugin({
     		filename: 'main.css',
